dc6026df431585fb459d72798ae00552ef65ed08
João Flávio Santos <joaoflavio.s@hotmail.com>
2022-06-21 02:10:35 -0300

    feat: config projects depedencies and fist Apollo query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
+import { ApolloProvider } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { client } from './lib/apollo'
 
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
 
 // TypeScript + JSX (XML dentro do Javascript)
+
+// TypeScript + JSX (XML dentro do Javascript)
+
+// CMS - Content Management System
+// Wordpress não é Headless, pois entrega tanto o painel de ADMIN quanto o Frontend (temas)
+// Headless CMS (GraphCMS): Painel de ADMIN (Dados fornecidos através de uma API REST ou GraphQL)
+// React consome essa API do CMS
