9d94d9800409945866da600b539141a89e11f749
João Flávio Santos <joaoflavio.s@hotmail.com>
2022-06-22 00:40:27 -0300

    feat: implementing firts pages and componentes (Header, Sidebar and Lesson)

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,13 @@
+export default function Lesson() {
+  return(
+    <a href="#">
+      <span>Terça • 22 de junho • 19h00</span>
+      <div>
+        <header>
+          <span>Conteúdo Liberado</span>
+        </header>
+        <strong>Aula 02 - Titulo aula ignite labs</strong>
+      </div>
+    </a>
+  )
+}
