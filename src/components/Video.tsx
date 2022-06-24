faa6bfdce35be854bbc9d9521ba4b4033e12e162
João Flávio Santos <joaoflavio.s@hotmail.com>
2022-06-24 03:33:36 -0300

    fix: route fix bug in production

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -94,11 +94,11 @@ export default function Video(props: VideoProps) {
             </div>
           </div>
           <div className="flex flex-col gap-4">
-            <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+            <a href="" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
               <DiscordLogo size={24} />
               Discord Community
             </a>
-            <a href="#" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900">
+            <a href="" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900">
               <Lightning size={24} />
               Access Challenge
             </a>
---
+++ src/pages/Subscribe.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+export default function Subscribe() {
+
+  return (
+      <div className="flex flex-col min-h-screen">
+        <Link to={`/event/`}>
+          Hello!
+        </Link>
+      </div>
+  )
+}
